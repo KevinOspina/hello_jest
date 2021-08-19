cb39eb23fa8d23aea575fbd7da371a0c0cfdad6e
Kevin Ospina Cañaveral <36991694+KevinOspina@users.noreply.github.com>
2021-08-18 19:20:19 -0500

    Update globalSetup.js

--- jest/globalSetup.js
+++ jest/globalSetup.js
@@ -7,7 +7,7 @@ module.exports = async function globalSetup(globalConfig) {
   await setupDevServer({
     command: 'http-server . -p 3000',
     launchTimeout: 10000,
-    port: 3000
+    port: 3001
   })
 
   // also do standard puppeteer setup
